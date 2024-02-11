69effc195e934a965a15ae6cb0931123a9cb7953
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2024-02-11 14:53:51 +0900

    :sparkles: 486. Predict the Winner

--- leetcode/486.js
+++ leetcode/486.js
@@ -1,12 +1,12 @@
 /**
- * 486. Predict the Winner (DFS, DP)
+ * 486. Predict the Winner (DFS)
  * https://leetcode.com/problems/predict-the-winner/description/
  *
  * @param {number[]} nums
  * @return {boolean}
  */
-var predictTheWinner = function(nums) {
-    const memo = Array.from({ length: nums.length }, () => new Array(nums.length).fill(-1));
+var predictTheWinner = function (nums) {
+    const memo = Array.from({length: nums.length}, () => new Array(nums.length).fill(-1));
 
     const dfs = (nums, start, end) => {
         if (start === end) {
@@ -33,5 +33,32 @@ var predictTheWinner = function(nums) {
     const player_2 = nums.reduce((acc, curr) => acc + curr, 0) - player_1;
 
     return player_1 >= player_2;
+};
+
+
+/**
+ * 486. Predict the Winner (DP)
+ * https://leetcode.com/problems/predict-the-winner/description/
+ *
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var predictTheWinner = function (nums) {
+    const n = nums.length;
+    // dp[i][j] = 배열의 i ~ j번째 요소를 고려했을 때 먼저 움직인 플레이어가 몇 점 더 얻을 수 있는지 나타내는 배열
+    const dp = Array.from({length: n}, () => new Array(n).fill(-1));
+
+    for (let i = 0; i < n; i++) {
+        dp[i][i] = nums[i];
+    }
+
+    for (let size = 1; size < n; size++) {
+        for (let i = 0; i < n - size; i++) {
+            const j = i + size;
+            dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+        }
+    }
+
+    return dp[0][n - 1] >= 0;
 };
 
