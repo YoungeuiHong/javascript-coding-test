bcb6f66aa586279b3482c76ab4a05cbff62dd5fb
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2024-02-21 00:38:51 +0900

    :sparkles: 130. Surrounded Regions

--- leetcode/130.js
+++ leetcode/130.js
@@ -57,3 +57,59 @@ const solve = function (board) {
         }
     }
 };
+
+
+/**
+ * 130. Surrounded Regions (DFS)
+ * https://leetcode.com/problems/surrounded-regions/
+ *
+ * @param {character[][]} board
+ * @return {void} Do not return anything, modify board in-place instead.
+ */
+var solve = function (board) {
+    const m = board.length;
+    const n = board[0].length;
+
+    function dfs(i, j) {
+        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] !== 'O') return;
+
+        board[i][j] = '#';
+        dfs(i - 1, j);
+        dfs(i + 1, j);
+        dfs(i, j - 1);
+        dfs(i, j + 1);
+    }
+
+    for (let i = 0; i < m; i++) {
+        if (board[i][0] === 'O') {
+            dfs(i, 0);
+        }
+
+        if (board[i][n - 1] === 'O') {
+            dfs(i, n - 1);
+        }
+    }
+
+    for (let j = 0; j < n; j++) {
+        if (board[0][j] === 'O') {
+            dfs(0, j);
+        }
+
+        if (board[m - 1][j] === 'O') {
+            dfs(m - 1, j);
+        }
+    }
+
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (board[i][j] === 'O') {
+                board[i][j] = 'X';
+            } else if (board[i][j] === '#') {
+                board[i][j] = 'O';
+            }
+        }
+    }
+
+    return board;
+};
