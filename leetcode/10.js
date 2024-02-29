1a94f176fac941ea5f59a5c90fc46f06d6b41a61
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2024-02-29 23:52:04 +0900

    :sparkles: 10. Regular Expression Matching

--- leetcode/10.js
+++ leetcode/10.js
@@ -20,6 +20,9 @@ var isMatch = function (s, p) {
     // empty string = empty pattern
     dp[0][0] = true;
 
+    // ex. s = aab, p = c*a*b라면 true가 나와야 한다. *는 선행문자가 한 번도 안 나와도 되기 때문이다.
+    // 따라서 #을 임의의 문자라 한다면 #*#*#*#*는 empty string ""와 부합한다.
+    // *는 짝수 번째에 위치해야 한다.
     for (let j = 2; j < pLen + 1; j += 2) {
         if (p[j - 1] === "*" && dp[0][j - 2]) {
             dp[0][j] = true;
@@ -32,6 +35,9 @@ var isMatch = function (s, p) {
                 dp[i][j] = dp[i - 1][j - 1];
             } else if (p[j - 1] === '*') {
                 if (p[j - 2] === s[i - 1] || p[j - 2] === '.') {
+                    // dp[i][j - 2] : * 앞의 문자가 0번 사용된 경우
+                    // dp[i - 1][j - 2] : * 앞의 문자가 한 번만 사용된 경우
+                    // dp[i - 1][j] : * 앞의 문자가 여러 번 사용된 경우
                     dp[i][j] = dp[i][j - 2] || dp[i - 1][j - 2] || dp[i - 1][j]
                 } else {
                     dp[i][j] = dp[i][j - 2];
