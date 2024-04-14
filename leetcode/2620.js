7dca2da98141e84c472b9e4da760e04edbf1f4e9
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2024-04-14 15:25:13 +0900

    :sparkles: 2620. Counter

---
+++ leetcode/2620.js
@@ -0,0 +1,21 @@
+/**
+ * 2620. Counter
+ * https://leetcode.com/problems/counter/description/?envType=study-plan-v2&envId=30-days-of-javascript
+ *
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function (n) {
+    let count = n;
+
+    return function () {
+        return count++;
+    };
+};
+
+/**
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
