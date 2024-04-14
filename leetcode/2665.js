0e3939ed3007bbcebc352f193505d955c046fef9
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2024-04-14 15:36:40 +0900

    :sparkles: 2665. Counter II

---
+++ leetcode/2665.js
@@ -0,0 +1,35 @@
+/**
+ * 2665. Counter II
+ * https://leetcode.com/problems/counter-ii/description/?envType=study-plan-v2&envId=30-days-of-javascript
+ *
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function (init) {
+    let val = init;
+
+    function increment() {
+        return ++val;
+    }
+
+    function decrement() {
+        return --val;
+    }
+
+    function reset() {
+        return val = init;
+    }
+
+    return {
+        increment,
+        decrement,
+        reset
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
