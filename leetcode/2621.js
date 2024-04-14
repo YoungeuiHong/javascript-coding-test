4dd7b10b15570086069bdc56c7b6c2034b6c8659
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2024-04-14 16:03:58 +0900

    :sparkles: 2621. Sleep

---
+++ leetcode/2621.js
@@ -0,0 +1,17 @@
+/**
+ * 2621. Sleep
+ * https://leetcode.com/problems/sleep/description/?envType=study-plan-v2&envId=30-days-of-javascript
+ *
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, millis, millis);
+    })
+}
+
+/**
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
