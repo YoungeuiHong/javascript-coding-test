ccdd5252a3b5d769691f7d7495e75175434fd64d
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2024-04-18 14:27:22 +0900

    :sparkles: 2627. Debounce

---
+++ leetcode/2627.js
@@ -0,0 +1,31 @@
+/**
+ * 2627. Debounce
+ * https://leetcode.com/problems/debounce/description/?envType=study-plan-v2&envId=30-days-of-javascript
+ *
+ * 디바운스는 짧은 시간 간격으로 이벤트가 연속해서 발생하면 이벤트 핸들러를 호출하지 않다가
+ * 일정 시간이 경과한 이후에 이벤트 핸들러가 한 번만 호출되도록 한다.
+ *
+ * 즉 디바운스는 짧은 시간 간격으로 발생하는 이벤트를 그룹화하여
+ *
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function (fn, t) {
+    let timerId;
+
+    // 디바운스는 함수는 timerId를 기억하는 클로저를 반환한다.
+    return function (...args) {
+        // 딜레이 t가 경과하기 전에 이벤트가 발생하면 이전 타이머를 취소하고 새로운 타이머를 설정한다.
+        // 따라서 딜레이보다 짧은 간격으로 이벤트가 발생하면 콜백함수 fn은 호출되지 않는다.
+        if (timerId) clearTimeout(timerId);
+        timerId = setTimeout(fn, t, ...args);
+    }
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
